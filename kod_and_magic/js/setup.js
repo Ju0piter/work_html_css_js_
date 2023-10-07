2e70a317ebd7c3d8d6549a31a69125e4b6a6d2c1
Ju0piter <dmitrij.britvin@yandex.ru>
2023-10-07 17:46:11 +0300

    KaM_update_3lection_17:45_07.10.23

--- kod_and_magic/js/setup.js
+++ kod_and_magic/js/setup.js
@@ -14,40 +14,49 @@ var Mass_coatColor = ["rgb(101, 137, 164)",
                       "rgb(56, 159, 117)",
                       "rgb(215, 210, 55)",
                       "rgb(0, 0, 0)"];
+
 var Mass_eyesColor = ["black",
                       "red",
                       "blue",
                       "yellow",
                       "green"];
+
+var Fun_random = function(mass){
+  var stroka = mass[Math.floor(Math.random() * mass.length)]
+  return stroka;
+};
+
+var Fun_Create_DOM = function(WizardCopy_El, wizzards_El){
+  var WizardEllement = WizardCopy_El.cloneNode(true);
+  WizardEllement.querySelector(".setup-similar-label").textContent = wizzards_El[i].name;
+  WizardEllement.querySelector(".wizard-coat").style.fill = wizzards_El[i].coatColor;
+  WizardEllement.querySelector(".wizard-eyes").style.fill = wizzards_El[i].eyesColor;
+  return WizardEllement;
+};
+
+var Fun_Insert_DOM = function(SimularList_El,WizardEllement_El) {
+  SimularList_El.appendChild(WizardEllement_El);
+};
+
 var wizzards = [
-    { name : Mass_name[Math.floor(Math.random() * Mass_name.length)] 
-             +" " + Mass_fam[Math.floor(Math.random() * Mass_fam.length)] ,
-     coatColor : Mass_coatColor[Math.floor(Math.random() * Mass_coatColor.length)] ,
-     eyesColor: Mass_eyesColor[Math.floor(Math.random() * Mass_eyesColor.length)]},
-    { name : Mass_name[Math.floor(Math.random() * Mass_name.length)] 
-        +" " + Mass_fam[Math.floor(Math.random() * Mass_fam.length)] ,
-      coatColor : Mass_coatColor[Math.floor(Math.random() * Mass_coatColor.length)] ,
-      eyesColor: Mass_eyesColor[Math.floor(Math.random() * Mass_eyesColor.length)]},
-    { name : Mass_name[Math.floor(Math.random() * Mass_name.length)] 
-        +" " + Mass_fam[Math.floor(Math.random() * Mass_fam.length)] ,
-      coatColor : Mass_coatColor[Math.floor(Math.random() * Mass_coatColor.length)] ,
-      eyesColor: Mass_eyesColor[Math.floor(Math.random() * Mass_eyesColor.length)]},
-    { name : Mass_name[Math.floor(Math.random() * Mass_name.length)] 
-        +" " + Mass_fam[Math.floor(Math.random() * Mass_fam.length)] ,
-      coatColor : Mass_coatColor[Math.floor(Math.random() * Mass_coatColor.length)] ,
-      eyesColor: Mass_eyesColor[Math.floor(Math.random() * Mass_eyesColor.length)]}
+    { name : Fun_random(Mass_name) + " " + Fun_random(Mass_fam) ,
+     coatColor : Fun_random(Mass_coatColor) ,
+     eyesColor: Fun_random(Mass_eyesColor)},
+     { name : Fun_random(Mass_name) + " " + Fun_random(Mass_fam) ,
+     coatColor : Fun_random(Mass_coatColor) ,
+     eyesColor: Fun_random(Mass_eyesColor)},
+     { name : Fun_random(Mass_name) + " " + Fun_random(Mass_fam) ,
+     coatColor : Fun_random(Mass_coatColor) ,
+     eyesColor: Fun_random(Mass_eyesColor)},
+     { name : Fun_random(Mass_name) + " " + Fun_random(Mass_fam) ,
+     coatColor : Fun_random(Mass_coatColor) ,
+     eyesColor: Fun_random(Mass_eyesColor)}
 ];
 console.log(wizzards);
 
 var SimularList = document.querySelector(".setup-similar-list"); 
 
 for (var i = 0;i<4;i++){
-
-    var WizardEllement = WizardCopy.cloneNode(true);
-    WizardEllement.querySelector(".setup-similar-label").textContent = wizzards[i].name;
-    WizardEllement.querySelector(".wizard-coat").style.fill = wizzards[i].coatColor;
-    WizardEllement.querySelector(".wizard-eyes").style.fill = wizzards[i].eyesColor;
-
-
-    SimularList.appendChild(WizardEllement);
+  var Create_DOM = Fun_Create_DOM(WizardCopy, wizzards);
+  Fun_Insert_DOM(SimularList,Create_DOM);
 };
